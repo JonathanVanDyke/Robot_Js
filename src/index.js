dee010d3c9f6d4ad9b4d9b1800b3ff8f8496511f
Jonathan <jfv213@gmail.com>
2019-10-17 10:34:27 -0400

    Update for heroku

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
-});
